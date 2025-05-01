ff608198a53946e042ff2df642599ba911ece5c9
Mhkans <harry45789@gmail.com>
2025-05-01 15:02:23 +0900

    스킬모듈화 2
    임시저장
    ASC에 동적태그 정적태그 추가 예정

--- CAS/Source/CAS/Ability/CAS_Ability_TESTFUNC.h
+++ CAS/Source/CAS/Ability/CAS_Ability_TESTFUNC.h
@@ -21,6 +21,8 @@ protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Montage")
 	TObjectPtr<UAnimMontage> AttackMontage;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Gameplay Effect")
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
 };
