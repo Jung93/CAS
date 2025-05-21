4b4a6fdc8d891129e4654759c0ba77acf3e69cd2
KMS <harry45789@gmail.com>
2025-05-21 18:46:36 +0900

    데드이벤트 01

--- CAS/Source/CAS/Ability_Task/CAS_Task_Dead.cpp
+++ CAS/Source/CAS/Ability_Task/CAS_Task_Dead.cpp
@@ -19,11 +19,36 @@ UCAS_Task_Dead* UCAS_Task_Dead::Task_Dead(UGameplayAbility* OwningAbility, FName
 void UCAS_Task_Dead::Activate()
 {
 	Super::Activate();
+
 	if (!Montage) {
 		return;
+	}
+	auto Character = Cast<ACAS_Character>((GetAvatarActor()));
+	if (Character->IsValidLowLevel()) {
+
+		UAnimInstance* AnimInstance = Character->GetMesh()->GetAnimInstance();
+
+		if (!AnimInstance)
+		{
+			return;
+		}
+
+		AnimInstance->OnPlayMontageNotifyBegin.AddDynamic(this, &ThisClass::PlayAnimNotify);
+		AnimInstance->Montage_Play(Montage, PlayRate);
 	}
 }
 
 void UCAS_Task_Dead::PlayAnimNotify(FName NotifyName, const FBranchingPointNotifyPayload& BranchingPointPayload)
 {
+
 }
+/*
+Todo
+체력이 0되면 델리게이트로 플레이어의 데드어빌리티 실행
+데드어빌리티 -> 즉시 데드 태그를 달고
+->태스크실행 ->애님노티파이로 사라지게처리->어빌리티 종료
+
+데드태그 -> 행동트리에서 막아야함 , 데드태그를 달면 하던공격취소 및 데미지 x
+
+
+*/
