e29597fa08ccd1b9529c40e6f83fb3627f6de673
Jung93 <105581479+Jung93@users.noreply.github.com>
2025-05-28 16:04:22 +0900

    불필요한 파일 삭제

--- CAS/Source/CAS/GAS/CAS_GamePlayTag.cpp
+++ CAS/Source/CAS/GAS/CAS_GamePlayTag.cpp
@@ -11,10 +11,10 @@ namespace CAS_GamePlayTag {
 	UE_DEFINE_GAMEPLAY_TAG(Ability_Move_Evade, "Ability.Move.Evade")
 	UE_DEFINE_GAMEPLAY_TAG(Ability_State_Dead, "Ability.State.Dead")
 	UE_DEFINE_GAMEPLAY_TAG(Ability_State_Invincible, "Ability.State.Invincible")
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Move_SuperJump, "Ability.Move.SuperJump")
 
 	UE_DEFINE_GAMEPLAY_TAG(Effect_Attack_TEST, "Effect.Attack.TEST")
 	UE_DEFINE_GAMEPLAY_TAG(Effect_Move_Evade, "Effect.Move.Evade")
-	UE_DEFINE_GAMEPLAY_TAG(Effect_Move_Evade, "Effect.Move.SuperJump")
 	UE_DEFINE_GAMEPLAY_TAG(Effect_Status_Stun, "Effect.Status.Stun")
 
 	UE_DEFINE_GAMEPLAY_TAG(State_TakeDamage, "State.TakeDamage")
