cf0058c515af178ff931f2cee6a56a5ebbc22fc6
KMS <harry45789@gmail.com>
2025-07-25 18:33:05 +0900

    인식 디버그용 머티리얼 변화 코드 추가

--- CAS/Source/CAS/Controller/CAS_EnemyController.cpp
+++ CAS/Source/CAS/Controller/CAS_EnemyController.cpp
@@ -25,6 +25,7 @@ ACAS_EnemyController::ACAS_EnemyController()
 void ACAS_EnemyController::OnPossess(APawn* pawn)
 {
 	Super::OnPossess(pawn);
+	SetMeshColor(GetPawn(), OriginalColorVector, "Tint");
 	BlackBoardComponent = Blackboard;
 	UseBlackboard(BlackboardData, BlackBoardComponent);
 	RunBehaviorTree(BehaviorTree);
@@ -32,6 +33,7 @@ void ACAS_EnemyController::OnPossess(APawn* pawn)
 
 void ACAS_EnemyController::OnUnPossess()
 {
+	SetMeshColor(GetPawn(), OriginalColorVector, "Tint");
 	Super::OnUnPossess();
 }
 
@@ -46,26 +48,43 @@ void ACAS_EnemyController::RandMove()
 }
 
 void ACAS_EnemyController::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
-{	
+{
 	if (Actor == nullptr || Cast<ACAS_Character>(Actor) == nullptr) {
+		return;
+	}
+	auto NPCpawn = GetPawn();
+	auto NPC = Cast<ACAS_Character>(NPCpawn);
+	if (NPCpawn == nullptr || NPC->GetAbilitySystemComponent()->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag("Effect.Status.Stun"))) {
 		return;
 	}
 	auto character = Cast<ACAS_Character>(Actor);
 
 	auto CharacterController = character->GetController();
+	auto playerController = Cast<ACAS_PlayerController>(CharacterController);
 
-	if (auto playerController = Cast<ACAS_PlayerController>(CharacterController)) {
+	if (!playerController) {
+		return;
+	}
+
+	if (Stimulus.WasSuccessfullySensed()) {
 		bool bDetected = character->GetAbilitySystemComponent()->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag("State.Detectable"));
-		
+
 		if (bDetected) {
 			BlackBoardComponent->SetValueAsBool("bPlayerDetected", true);
 			BlackBoardComponent->SetValueAsObject("Player", Actor);
 			BlackBoardComponent->SetValueAsVector("MovePosition", Actor->GetActorLocation());
+
+			FVector ColorVector(1, 0, 1);
+			SetMeshColor(character, ColorVector);
+
 			GetWorld()->GetTimerManager().SetTimer(TrackingTimerHandle, this, &ThisClass::StopTrackingPlayer, TrackingTime, false);
 		}
-				
 	}
-	
+	else {
+		FVector ColorVector(0, 0, 0);
+		SetMeshColor(character, ColorVector);
+	}
+
 }
 
 void ACAS_EnemyController::StopTrackingPlayer()
@@ -76,4 +95,22 @@ void ACAS_EnemyController::StopTrackingPlayer()
 		BlackBoardComponent->SetValueAsVector("MovePosition", GetPawn()->GetActorLocation());
 	}
 	GetWorld()->GetTimerManager().ClearTimer(TrackingTimerHandle);
+}
+
+
+void ACAS_EnemyController::SetMeshColor(APawn* pawn, FVector colorVector, FName name)
+{
+	auto Mesh = Cast<ACharacter>(pawn)->GetMesh();
+
+	UMaterialInstanceDynamic* DynMat0 = Mesh->CreateDynamicMaterialInstance(0);
+	if (DynMat0)
+	{
+		DynMat0->SetVectorParameterValue(name, colorVector);
+	}
+
+	UMaterialInstanceDynamic* DynMat1 = Mesh->CreateDynamicMaterialInstance(1);
+	if (DynMat1)
+	{
+		DynMat1->SetVectorParameterValue(name, colorVector);
+	}
 }
