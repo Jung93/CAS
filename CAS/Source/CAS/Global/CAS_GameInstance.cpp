e5885f3f3efbc8efe0009aed6667d92e0c5afbaf
Mhkans <harry45789@gmail.com>
2025-08-06 20:17:01 +0900

    임시저장

--- CAS/Source/CAS/Global/CAS_GameInstance.cpp
+++ CAS/Source/CAS/Global/CAS_GameInstance.cpp
@@ -3,3 +3,34 @@
 
 #include "Global/CAS_GameInstance.h"
 
+UCAS_GameInstance::UCAS_GameInstance()
+{
+}
+
+void UCAS_GameInstance::Init()
+{
+	Super::Init();
+
+}
+
+FString UCAS_GameInstance::GetConfig(const FString& key, const FString& value) const
+{
+	return FString();
+}
+
+void UCAS_GameInstance::SetConfig(const FString& key, FString& value)
+{
+}
+
+void UCAS_GameInstance::LoadSettingFromINI()
+{
+}
+
+void UCAS_GameInstance::SaveSettingToINI()
+{
+}
+
+FString UCAS_GameInstance::GetSettingPath() const
+{
+	return FString();
+}
--- CAS/Source/CAS/Global/CAS_GameInstance.h
+++ CAS/Source/CAS/Global/CAS_GameInstance.h
@@ -13,5 +13,22 @@ UCLASS()
 class CAS_API UCAS_GameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+public:
+	UCAS_GameInstance();
+
+	virtual void Init() override;
+
+	UFUNCTION()
+	FString GetConfig(const FString& key, const FString& value) const;
+	UFUNCTION()
+	void SetConfig(const FString& key, FString& value);
+	UFUNCTION()
+	void LoadSettingFromINI();	
+	UFUNCTION()
+	void SaveSettingToINI();						
+
+	FString GetSettingPath()const;
+
+	UPROPERTY(VisibleAnywhere, Category = "Setting")
+	TMap<FString, FString> GameSetting;
 };
