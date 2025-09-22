38dd3f75430223b9e2bda086fa0235f4c46590ad
KMS <harry45789@gmail.com>
2025-09-22 18:49:18 +0900

    te

--- CAS/Source/CAS/Character/NPC/CAS_SaveNPC.cpp
+++ CAS/Source/CAS/Character/NPC/CAS_SaveNPC.cpp
@@ -12,39 +12,13 @@
 // Sets default values
 ACAS_SaveNPC::ACAS_SaveNPC()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
 
-	SenseCollider = CreateDefaultSubobject<USphereComponent>(TEXT("Collider"));
-	NPC_Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
-	KeyPressWidgetComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("KeyPressWidgetComponent"));
-	RootComponent = NPC_Mesh;
-
-	SenseCollider->SetupAttachment(NPC_Mesh);
-	KeyPressWidgetComponent->SetupAttachment(NPC_Mesh);
-	SenseCollider->SetCollisionProfileName(TEXT("NPC"));
 }
 
 // Called when the game starts or when spawned
 void ACAS_SaveNPC::BeginPlay()
 {
 	Super::BeginPlay();
-
-	if (KeyPressUIClass) {
-		KeyPressWidgetComponent->SetWidgetClass(KeyPressUIClass);
-		KeyPressWidgetComponent->SetWidgetSpace(EWidgetSpace::Screen);
-		KeyPressWidgetComponent->SetVisibility(true);
-
-		KeyPressWidgetComponent->SetRelativeLocation(FVector(0, 0, 88.0f));
-	}
-	KeyPressUI = Cast<UCAS_KeyPressUI>(KeyPressWidgetComponent->GetUserWidgetObject());
-	if (KeyTexture && KeyPressUI) {
-		KeyPressUI->SetTexture(KeyTexture);
-		KeyPressUI->SetVisibility(ESlateVisibility::Collapsed);
-	}
-
-	SenseCollider->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnOverlapEvent);
-	SenseCollider->OnComponentEndOverlap.AddDynamic(this, &ThisClass::EndOverlapEvent);
 
 	SaveLoadWidget = CreateWidget<UCAS_SaveLoadWidget>(GetWorld(), SaveLoadWidgetClass);
 	SaveLoadWidget->bSaveMode = true;
@@ -60,24 +34,19 @@ void ACAS_SaveNPC::Tick(float DeltaTime)
 
 }
 
+void ACAS_SaveNPC::OnOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnOverlapEvent(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+}
+
+void ACAS_SaveNPC::EndOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Super::EndOverlapEvent(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+	SaveLoadWidget->CloseSaveLoadWidget();
+}
+
 void ACAS_SaveNPC::InteractionWithPlayer()												
 {
 	SaveLoadWidget->DisplaySaveLoadWidget();
 }
 
-void ACAS_SaveNPC::OnOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	if (KeyPressUI) {
-		KeyPressUI->SetVisibility(ESlateVisibility::Visible);
-		bCanInteraction = true;
-	}
-}
-
-void ACAS_SaveNPC::EndOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-	if (KeyPressUI) {
-		KeyPressUI->SetVisibility(ESlateVisibility::Collapsed);
-		bCanInteraction = false;
-	}
-	SaveLoadWidget->CloseSaveLoadWidget();
-}
---
+++ CAS/Source/CAS/InteractionActor/CAS_InteractionActor.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CAS_InteractionActor.generated.h"
+
+UCLASS()
+class CAS_API ACAS_InteractionActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACAS_InteractionActor();
+
+	bool CanInteraction() { return bCanInteraction; }
+	virtual void InteractionWithPlayer() PURE_VIRTUAL(ACAS_InteractionActor::InteractionWithPlayer, ;); 
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+protected:
+	UFUNCTION()
+	virtual void OnOverlapEvent(class UPrimitiveComponent* OverlappedComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const struct FHitResult& SweepResult);
+	UFUNCTION()
+	virtual void EndOverlapEvent(class UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY(EditAnywhere, Category = "Collider")
+	class USphereComponent* SenseCollider;
+
+protected:
+	//상호작용
+	UPROPERTY(EditAnywhere, Category = "UI")
+	TSubclassOf<class UCAS_KeyPressUI> KeyPressUIClass;
+	UPROPERTY()
+	class UCAS_KeyPressUI* KeyPressUI = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "UI")
+	class UWidgetComponent* KeyPressWidgetComponent;
+	UPROPERTY(EditAnywhere, Category = "UI")
+	class UTexture2D* KeyTexture;
+
+	bool bCanInteraction = false;
+
+protected:
+	//mesh
+	UPROPERTY(EditAnywhere, Category = "Mesh")
+	class UStaticMeshComponent* StaticMesh;
+
+};
