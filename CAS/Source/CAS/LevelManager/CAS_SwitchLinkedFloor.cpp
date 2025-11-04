37c670123c907a4c64a83658e13e397365661b86
KMS <harry45789@gmail.com>
2025-11-04 18:29:44 +0900

    퍼즐맵 임시저장

---
+++ CAS/Source/CAS/LevelManager/CAS_SwitchLinkedFloor.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LevelManager/CAS_SwitchLinkedFloor.h"
+
+ACAS_SwitchLinkedFloor::ACAS_SwitchLinkedFloor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	FloorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FloorMesh"));
+
+}
+
+void ACAS_SwitchLinkedFloor::SwitchClicked(bool SwitchOn)
+{
+	bSwitchOn = SwitchOn;
+}
+
+void ACAS_SwitchLinkedFloor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	int32 ActorNum = Actors.Num();
+
+	if (FMath::IsNearlyZero(Radius)) {
+		Radius = FloorMesh->Bounds.BoxExtent.X * Param;
+	}
+	
+	for (int32 i = 0; i < ActorNum; i++)
+	{
+		float Angle = i * (360.0f / ActorNum);
+		float RadianAngle = FMath::DegreesToRadians(Angle);
+
+		FVector ActorPosition = GetActorLocation() + FVector(FMath::Cos(RadianAngle), FMath::Sin(RadianAngle), 0) * Radius;
+
+		ActorLocation.Add(ActorPosition);
+		Actors[i]->SetActorLocation(ActorLocation[i]);
+		Actors[i]->AttachToActor(this, FAttachmentTransformRules::KeepWorldTransform);
+	}
+}
+
+void ACAS_SwitchLinkedFloor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bSwitchOn) {
+		return;
+	}
+	else {
+		if (bInverse) {
+			AddActorLocalRotation(FRotator(0, -Degree, 0));
+		}
+		else {
+			AddActorLocalRotation(FRotator(0, Degree, 0));
+		}
+	}
+}
