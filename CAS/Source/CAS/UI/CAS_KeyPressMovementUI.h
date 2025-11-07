5be8e59a6ff606de9f2466e4bfd92392999f3fd7
KMS <harry45789@gmail.com>
2025-11-07 17:48:57 +0900

    거울조작용 ui 구현 준비

---
+++ CAS/Source/CAS/UI/CAS_KeyPressMovementUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/CAS_KeyPressUI.h"
+#include "CAS_KeyPressMovementUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAS_API UCAS_KeyPressMovementUI : public UCAS_KeyPressUI
+{
+	GENERATED_BODY()
+	
+};
