1c3d9b2b43306684b3f351115041ea8f6d95ed34
Mhkans <harry45789@gmail.com>
2025-11-28 14:56:48 +0900

    타겟미러 추가

---
+++ CAS/Source/CAS/InteractionActor/CAS_InteractionTargetMirror.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractionActor/CAS_InteractionTargetMirror.h"
+#include "LevelManager/CAS_SwitchLinkedFloor.h"
+#include "Kismet/KismetMathLibrary.h"
+
+ACAS_InteractionTargetMirror::ACAS_InteractionTargetMirror()
+{
+}
+
+void ACAS_InteractionTargetMirror::BeginPlay()
+{
+	//위치정보만 받아와서저장
+
+	Super::BeginPlay();
+
+	const auto& TargetActors = TargetFloor->GetOwningActors();
+
+	for (auto TargetActor : TargetActors) {
+		FVector ActorLocation = TargetActor->GetActorLocation();
+		if (ActorLocation == GetActorLocation()) {
+			continue;
+		}
+		TargetActorLocation.Add(ActorLocation);
+	}
+	ArrayNum = TargetActors.Num();
+}
+
+void ACAS_InteractionTargetMirror::WidgetClicked(EWidgetPositionType Type)
+{
+	//좌우 레이저 타겟 정해서 쏘기
+
+	if (Type == EWidgetPositionType::NONE) {
+		return;
+	}
+	else if (Type == EWidgetPositionType::Left) {
+		TargetIndex--;
+
+		if (TargetIndex < 0) {
+			TargetIndex += ArrayNum;
+		}
+	}
+	else if (Type == EWidgetPositionType::Right) {
+		TargetIndex++;
+		
+		TargetIndex %= ArrayNum;
+	}
+	
+	FRotator LookAtRot = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), TargetActorLocation[TargetIndex]);
+
+	SetActorRotation(LookAtRot);
+}
---
+++ CAS/Source/CAS/InteractionActor/CAS_InteractionTargetMirror.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractionActor/CAS_InteractionMirror.h"
+#include "CAS_InteractionTargetMirror.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAS_API ACAS_InteractionTargetMirror : public ACAS_InteractionMirror
+{
+	GENERATED_BODY()
+public:
+	ACAS_InteractionTargetMirror();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void WidgetClicked(EWidgetPositionType Type) override;
+protected:
+	UPROPERTY(EditAnywhere)
+	class ACAS_SwitchLinkedFloor* TargetFloor;
+
+	UPROPERTY()
+	TArray<FVector> TargetActorLocation;
+
+	int32 TargetIndex = 0;
+	int32 ArrayNum = 0;
+};
